5386a3b1198a37de81a7dad16b3b82d437152d11
Myster7494 <jackychiu0101@gmail.com>
2024-12-24 20:30:45 +0800

    feat: square_number

--- 01_static_member/02_square_number/src/JAVA.java
+++ 01_static_member/02_square_number/src/JAVA.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class JAVA {
     // 任務描述：
-    // 請撰寫一個函式或靜態方法，使其能接收一個整數 x (-10^9 < x < 10^9) 並回傳其相反數。
+    // 請撰寫一個函式或靜態方法，使其能接收一個整數 x (-10^9 < x < 10^9) 並回傳其平方。
 
     // 不要更改這裡。 >>>>>
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        System.out.println(getOppositeNumber(n));
+        System.out.println(getSquareNumber(n));
     }
     // <<<<< 不要更改這裡。
 
