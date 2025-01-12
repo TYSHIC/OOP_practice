b73b362fccec05d0e227b9193bcddbef0e0c51c4
Myster7494 <jackychiu0101@gmail.com>
2025-01-13 00:12:42 +0800

    feat: climb_stairs

---
+++ 01_static_member/03_climb_stairs/src/JAVA.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class JAVA {
+    // 任務描述：
+    // 棕聖喜歡爬樓梯，一次可以爬一階或爬兩階，試問棕聖從樓梯下爬到第 n 階 (1 <= n <= 30) 有幾種爬法？
+
+    // 不要更改這裡。 >>>>>
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(getMethodCounts(n));
+    }
+    // <<<<< 不要更改這裡。
+
+    // 請在下方撰寫程式碼。
+
+}
